4777872723fbbf1c45a8bfb5273bfcda0502197a
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-01-23 10:39:11 -0600

    init: task2

--- script.js
+++ script.js
@@ -10,19 +10,44 @@ const personalMovieDB = {
   privat: false,
 };
 
-let lastFilmOne = prompt("Один из последних просмотренных фильмов?", ""),
-  ratingFilmOne = prompt("На сколько оцените его?", ""),
-  lastFilmTwo = prompt("Один из последних просмотренных фильмов?", ""),
-  ratingFilmTwo = prompt("На сколько оцените его?", "");
+const questonsList = () => {
+  for (let i = 0; i < 2; i++) {
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", ""),
+      ratingFilm = prompt("На сколько оцените его?", "");
+    if (
+      lastFilm != null &&
+      ratingFilm != null &&
+      lastFilm != "" &&
+      ratingFilm != "" &&
+      lastFilm.length < 50 &&
+      ratingFilm.length < 50
+    ) {
+      personalMovieDB.movies[lastFilm] = ratingFilm;
+    //   console.log("done!");
+    } else {
+    //   console.log("error!");
+      i--;
+    }
+  }
+};
 
-personalMovieDB.movies[lastFilmOne] = ratingFilmOne;
-personalMovieDB.movies[lastFilmTwo] = ratingFilmTwo;
+const rankUser = () => {
+  if (personalMovieDB.count < 10) {
+    console.log("Просмотрено довольно мало фильмов!");
+  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель!");
+  } else if (personalMovieDB.count >= 30) {
+    console.log("Вы киноман!");
+  } else {
+    console.log("Произошла ошибка!");
+  }
+};
 
+questonsList();
+rankUser();
 console.log(personalMovieDB);
 
-
-
-/* Задание на урок:
+/* Задание на урок 1:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
@@ -43,4 +68,20 @@ console.log(personalMovieDB);
         'logan': '8.1'
     }
 
-Проверить, чтобы все работало без ошибок в консоли */
+Проверить, чтобы все работало без ошибок в консоли 
+
+Задание на урок 2:
+
+1) Автоматизировать вопросы пользователю про фильмы при помощи цикла
+
+2) Сделать так, чтобы пользователь не мог оставить ответ в виде пустой строки,
+отменить ответ или ввести название фильма длинее, чем 50 символов. Если это происходит - 
+возвращаем пользователя к вопросам опять
+
+3) При помощи условий проверить  personalMovieDB.count, и если он меньше 10 - вывести сообщение
+"Просмотрено довольно мало фильмов", если от 10 до 30 - "Вы классический зритель", а если больше - 
+"Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
+
+4) Потренироваться и переписать цикл еще двумя способами
+
+*/

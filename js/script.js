bfe0680d3b54052fe3f38b732b35b58c7e96652f
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-01-29 06:56:50 +0300

    init: movie-app - task 2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,99 @@
-/* Задания на урок:
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+
+    const adv = document.querySelectorAll('.promo__adv img'),
+        poster = document.querySelector('.promo__bg'),
+        genre = poster.querySelector('.promo__genre'),
+        movieList = document.querySelector('.promo__interactive-list'),
+        addForm = document.querySelector('form.add'),
+        addInput = addForm.querySelector('.adding__input'),
+        checkbox = addForm.querySelector('[type="checkbox"]');
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            if (favorite) {
+                console.log("Добавляем любимый фильм");
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovieList(movieDB.movies, movieList);
+        }
+
+        event.target.reset();
+
+    });
+
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+    };
+
+    const makeChanges = () => {
+        genre.textContent = 'драма';
+
+        poster.style.backgroundImage = 'url("img/bg.jpg")';
+    };
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+    function createMovieList(films, parent) {
+        parent.innerHTML = "";
+        sortArr(films);
+    
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+                <li class="promo__interactive-item">${i + 1} ${film}
+                    <div class="delete"></div>
+                </li>
+            `;
+        });
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+
+                createMovieList(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    createMovieList(movieDB.movies, movieList);
+
+});
+
+
+/* 
+
+Задания на урок 1:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
 
@@ -10,36 +105,25 @@
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
 
-5) Добавить нумерацию выведенных фильмов */
+5) Добавить нумерацию выведенных фильмов 
 
-'use strict';
+______
 
-const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
-};
+Задания на урок 2:
 
-const adv = document.querySelectorAll('.promo__adv img'),
-      promoGenre = document.querySelector('.promo__genre'),
-      poster = document.querySelector('.promo__bg'),
-      promoFilmList = document.querySelectorAll('.promo__interactive-item');
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
 
-adv.forEach (item => { //1) Удалить все рекламные блоки со страницы (правая часть сайта)
-    item.remove();
-});
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
 
-promoGenre.textContent = 'драма'; //2) Изменить жанр фильма, поменять "комедия" на "драма"
-poster.style.cssText = 'background: url(../img/bg.jpg) center center/cover no-repeat;'; // 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img. Реализовать только при помощи JS
-movieDB.movies.sort(); // 4) Список фильмов на странице сформировать на основании данных из этого JS файла. Отсортировать их по алфавиту 
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
 
-function showCinemaList() {
-    for (let i = 0; i < movieDB.movies.length; i++) {  //5) Добавить нумерацию выведенных фильмов */
-        promoFilmList[i].textContent = `${i+1}. ${movieDB.movies[i]}`;
-    }
-}
-showCinemaList();
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту
+
+*/
